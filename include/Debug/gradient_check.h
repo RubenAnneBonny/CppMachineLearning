3917c7400b6d598a597c547985858ed8a35b4623
RubenAnneBonny <ruben.rystedt@icloud.com>
2026-07-18 21:34:52 +0200

    Changed Gradient check to only use double, to ensure reliable default parameters

--- include/Debug/gradient_check.h
+++ include/Debug/gradient_check.h
@@ -3,38 +3,35 @@
 
 #include <Func/function.h>
 #include <LinAlg/tensor.h>
+#include <Rand/random.h>
 #include <cmath>
 
 namespace Debug {
     namespace helper {
-        template <std::floating_point T, 
-                  std::invocable Eval>
-        std::pair<T, int> max_rel_error(LinAlg::Tensor<T>& disturb,
-                                        const LinAlg::Tensor<T>& analytic,
-                                        Eval evaluate,
-                                        T eps) {
-            T max_rel_error {};
+        template <std::invocable Eval>
+        std::pair<double, int> max_rel_error(LinAlg::Tensor<double>& disturb, const LinAlg::Tensor<double>& analytic, Eval evaluate, double eps) {
+            double max_rel_error {};
             int worst_index {-1};
 
             for(int i {}; i < disturb.get_extent(1); ++i) {
-                T original {disturb[{0, i}]};
+                double original {disturb[{0, i}]};
 
                 disturb[{0, i}] = original + eps;
-                T up {evaluate()};
+                double up {evaluate()};
 
                 disturb[{0, i}] = original - eps;
-                T down {evaluate()};
+                double down {evaluate()};
 
                 disturb[{0, i}] = original;
 
-                T numeric_val {(up - down) / (2 * eps)};
-                T analytic_val {analytic[{0, i}]};
+                double numeric_val {(up - down) / (2 * eps)};
+                double analytic_val {analytic[{0, i}]};
 
-                T max = 1e-6;
+                double max = 1e-6;
                 max = (max > std::abs(numeric_val) ? max : std::abs(numeric_val));
                 max = (max > std::abs(analytic_val) ? max : std::abs(analytic_val));
 
-                T rel_error {std::abs(numeric_val - analytic_val) / max};
+                double rel_error {std::abs(numeric_val - analytic_val) / max};
 
                 if(rel_error > max_rel_error) {
                     max_rel_error = rel_error;
@@ -47,33 +44,32 @@ namespace Debug {
     }
 
     /// @brief A struct of valuable info about the result of gradient check for functions
-    /// @tparam T A floating point type
-    template <std::floating_point T>
     struct Func_grad_check_result {
         bool passed = false;
-        T max_func_rel_error = 0;
-        T max_weights_rel_error = 0;
+        double max_func_rel_error = 0;
+        double max_weights_rel_error = 0;
         int worst_func_index = -1;
         int worst_weights_index = -1;
     };
 
     /// @brief Numerically tests a function to look for flaws in gradient calculation
-    /// @tparam T A floating point type
     /// @tparam F The function to test
     /// @param input_size The size of input, don't need to be large if speed is necessary
+    /// @param seed The seed, to ensure reproducability
     /// @param eps Step size disturbation, error in result is O(eps^2)
     /// @param rel_tol The relative tolerance, controls the passed variable in result
     /// @return A Func_grad_check_result instance
-    template <std::floating_point T,
-              Func::Function<T> F>
-    Func_grad_check_result<T> func_grad_check(int input_size, T eps = 1e-5, T rel_tol = 1e-4) {
-        LinAlg::Tensor<T> input {{1, input_size}};
-        LinAlg::Tensor<T> weights {{1, F::num_weights(input_size)}};
+    template <Func::Function<double> F>
+    Func_grad_check_result func_grad_check(int input_size, unsigned seed = 42, double eps = 1e-5, double rel_tol = 1e-6) {
+        LinAlg::Tensor<double> input {{1, input_size}};
+        LinAlg::Tensor<double> weights {{1, F::num_weights(input_size)}};
 
-        input.normal(0, 1);
-        weights.normal(0, 1);
+        Rand::Random<double> random {seed};
 
-        Func_grad_check_result<T> result;
+        input.normal(random, 0, 1);
+        weights.normal(random, 0, 1);
+
+        Func_grad_check_result result;
 
         auto eval {
             [&]()
@@ -82,10 +78,10 @@ namespace Debug {
             }
         };
 
-        LinAlg::Tensor<T> analytic_grad {F::function_grad(input, weights)};
+        LinAlg::Tensor<double> analytic_grad {F::function_grad(input, weights)};
         auto [func_rel_error, func_index] = helper::max_rel_error(input, analytic_grad, eval, eps);
 
-        LinAlg::Tensor<T> analytic_weight_grad {F::weights_grad(input, weights)};
+        LinAlg::Tensor<double> analytic_weight_grad {F::weights_grad(input, weights)};
         auto [weight_rel_error, weight_index] = helper::max_rel_error(weights, analytic_weight_grad, eval, eps);
 
         result.max_func_rel_error = func_rel_error;
@@ -98,36 +94,35 @@ namespace Debug {
     }
 
     /// @brief A struct of valuable info about the result of gradient chekc for activation functions
-    /// @tparam T A floating point type
-    template <std::floating_point T>
     struct Activation_grad_check_result {
         bool passed = false;
-        T max_rel_error = 0;
+        double max_rel_error = 0;
         int worst_index = -1;
     };
 
     /// @brief Numerically tests a activation function to look for flaws in gradient calculation
-    /// @tparam T A floating point type
     /// @tparam F The function to test
     /// @param input_size The size of input, don't need to be large if speed is necessary
+    /// @param seed The seed to ensure reproducability
     /// @param eps Step size disturbation, error in result is O(eps^2)
     /// @param rel_tol The relative tolerance, controls the passed variable in result
     /// @return A Activation_grad_check_result instance
-    template <std::floating_point T,
-              Func::Activation_function<T> A>
-    Activation_grad_check_result<T> activation_grad_check(int input_size, T eps = 1e-5, T rel_tol = 1e-4) {
-        LinAlg::Tensor<T> input {{1, input_size}};
+    template <Func::Activation_function<double> A>
+    Activation_grad_check_result activation_grad_check(int input_size, unsigned seed = 42, double eps = 1e-5, double rel_tol = 1e-6) {
+        LinAlg::Tensor<double> input {{1, input_size}};
 
-        input.normal(0, 1);
+        Rand::Random<double> random {seed};
 
-        T margin {1e-2};
+        input.normal(random, 0, 1);
+
+        double margin {1e-2};
         for(int i {}; i < input_size; ++i) {
             if(std::abs(input[{0, i}]) < margin) {
                 input[{0, i}] += (input[{0, i}] >= 0 ? margin : -margin);
             }
         }
 
-        Activation_grad_check_result<T> result;
+        Activation_grad_check_result result;
 
         auto eval {
             [&]()
@@ -136,7 +131,7 @@ namespace Debug {
             }
         };
 
-        LinAlg::Tensor<T> analytic_grad {A::derivate(input)};
+        LinAlg::Tensor<double> analytic_grad {A::derivate(input)};
         auto [rel_error, index] = helper::max_rel_error(input, analytic_grad, eval, eps);
 
         result.max_rel_error = rel_error;
