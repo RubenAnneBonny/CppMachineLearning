a21aa93b4fedc8ad525780961e641fb59514fdf4
Ruben Rystedt <rrystedt@kth.se>
2026-07-21 12:09:03 +0200

    Added optimizer check to debug

---
+++ include/Debug/optimizer_check.h
@@ -0,0 +1,87 @@
+#include <NN/optimizer.h>
+#include <NN/parameter.h>
+
+namespace Debug {
+    struct Optimizer_result {
+        bool passed = false;
+        bool descent_check = false;
+        bool parameter_coverage = false;
+        bool zero_gradient = false;
+    };
+
+    template <NN::Optimizer<double> Opt>
+    Optimizer_result optimizer_check(Opt optimizer) {
+        Optimizer_result result;
+        Rand::Random<double> random {};
+
+        {
+            Opt opt {optimizer};
+            NN::Parameter<double> param {{3, 4}};
+            param.normal(random, 0, 1);
+
+            param.grad = 2 * param.value;
+
+            LinAlg::Tensor<double> before = param.value.copy();
+            std::vector<NN::Parameter<double>*> params {&param};
+            opt.init(params);
+            opt.step();
+
+            LinAlg::Tensor<double> delta = param.value - before;
+            double dot = LinAlg::pairwise_mult<double>(delta, param.grad).sum();
+            result.descent_check = (dot < 0);
+        }
+
+        {
+            Opt opt {optimizer};
+            std::vector<NN::Parameter<double>> params {};
+
+            for(int i {}; i < 5; ++i) {
+                params.push_back(NN::Parameter<double>({3, 4}));
+                params[i].normal(random, 0, 1);
+                LinAlg::Tensor<double> A {{3, 4}};
+                A.normal(random, 0, 1);
+                params[i].grad = A;
+            }
+
+            std::vector<LinAlg::Tensor<double>> before {};
+
+            for(int i {}; i < 5; ++i) {
+                before.push_back(params[i].value.copy());
+            }
+
+            std::vector<NN::Parameter<double>*> pparams(5);
+            for(int i {}; i < 5; ++i) {
+                pparams[i] = &params[i];
+            }
+
+            opt.init(pparams);
+            opt.step();
+
+            result.parameter_coverage = true;
+
+            for(int i {}; i < 5; ++i) {
+                if(params[i].value == before[i]) {
+                    result.parameter_coverage = false;
+                }
+            }
+        }
+
+        {
+            Opt opt {optimizer};
+            NN::Parameter<double> param {{3, 4}};
+            param.normal(random, 0, 1);
+            param.zero_grad();
+
+            LinAlg::Tensor<double> before {param.value.copy()};
+            std::vector<NN::Parameter<double>*> params {&param};
+            opt.init(params);
+            opt.step();
+
+            result.zero_gradient = LinAlg::all_close<double>(param.value, before, 1e-7, 1e-7);
+        }
+
+        result.passed = result.zero_gradient && result.descent_check && result.parameter_coverage;
+
+        return result;
+    }   
+}
--- tests/NN/std_opt_test.cpp
+++ tests/NN/std_opt_test.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-TEST(Optimizer, GradientDescent) {
+TEST(Optimizer, GDStep) {
     NN::Parameter<float> param {{1, 3}, 1};
     param.value[{0, 1}] = 5;
     param.value[{0, 2}] = -2;
@@ -25,4 +25,36 @@ TEST(Optimizer, GradientDescent) {
     val_exp[{0, 2}] = 8;
 
     EXPECT_EQ(param.value, val_exp);
+}
+
+TEST(Optimizer, GDMultipleParameters) {
+    NN::Parameter<float> param_1 {{1, 3}, 1};
+    param_1.value[{0, 1}] = 5;
+    param_1.value[{0, 2}] = -2;
+
+    param_1.grad[{0, 0}] = 8;
+    param_1.grad[{0, 2}] = -5;
+
+    NN::Parameter<float> param_2 {{1, 2}, 1};
+    param_2.value[{0, 0}] = 3;
+    param_2.grad[{0, 1}] = -2;
+
+    NN::Gradient_descent<float> gd {2};
+
+    std::vector<NN::Parameter<float>*> params;
+    params.push_back(&param_1);
+    params.push_back(&param_2);
+
+    gd.init(params);
+    gd.step();
+
+    LinAlg::Tensor<float> p1_exp {{1, 3}, -15};
+    p1_exp[{0, 1}] = 5;
+    p1_exp[{0, 2}] = 8;
+
+    LinAlg::Tensor<float> p2_exp {{1, 2}, 3};
+    p2_exp[{0, 1}] = 5;
+
+    EXPECT_EQ(param_1.value, p1_exp);
+    EXPECT_EQ(param_2.value, p2_exp);
 }
