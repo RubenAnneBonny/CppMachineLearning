cd21a66261aab6750dbdc37076208b701fadc82a
Ruben Rystedt <rrystedt@kth.se>
2026-07-21 09:21:40 +0200

    Added more tests to tensor class

--- tests/LinAlg/tensor_test.cpp
+++ tests/LinAlg/tensor_test.cpp
@@ -324,6 +324,17 @@ TEST(Tensor, Argmax) {
     A[{1, 2}] = -7;
 
     EXPECT_EQ(A.argmax(), std::vector<int>({0, 1}));
+
+    LinAlg::Tensor<float> B {{2, 2}, -1};
+    B[{0, 0}] = -8;
+    B[{0, 1}] = -3;
+    B[{1, 1}] = -27;
+
+    EXPECT_EQ(B.argmax(), std::vector<int>({1, 0}));
+
+    LinAlg::Tensor<float> C {{2, 2, 2}, 6};
+
+    EXPECT_EQ(C.argmax(), std::vector<int>({0, 0, 0}));
 }
 
 TEST(Tensor, PairwiseSubtraction) {
@@ -601,4 +612,286 @@ TEST(Tensor, AllCloseShapeMismatch) {
     LinAlg::Tensor<float> B {{2, 2, 2}, 1};
 
     EXPECT_FALSE(LinAlg::all_close<float>(A, B, 1, 1));
+}
+
+TEST(Tensor, OneHot) {
+    LinAlg::Tensor<float> A {LinAlg::one_hot<float>(4, 2)};
+    LinAlg::Tensor<float> A_exp {{4}};
+    A_exp[{2}] = 1;
+
+    EXPECT_EQ(A, A_exp);
+}
+
+TEST(Tensor, IndependentCopy) {
+    LinAlg::Tensor<float> A {{2, 3}, 1};
+    A[{0, 2}] = 3;
+
+    LinAlg::Tensor<float> B {A.copy()};
+
+    A[{0, 1}] = 4;
+    B[{0, 1}] = -1;
+
+    EXPECT_NE(A, B);
+}
+
+TEST(Tensor, UnsqueezeIsView) {
+    LinAlg::Tensor<float> A {{2}, 1};
+    LinAlg::Tensor<float> A_unsqueezed {A.unsqueeze()};
+
+    A_unsqueezed[{0, 1}] = 3;
+
+    LinAlg::Tensor<float> A_exp {{2}, 1};
+    A_exp[{1}] = 3;
+
+    EXPECT_EQ(A, A_exp);
+}
+
+TEST(Tensor, SqueezeIsView) {
+    LinAlg::Tensor<float> A {{1, 2}, 1};
+    LinAlg::Tensor<float> A_squeezed {A.squeeze()};
+
+    A_squeezed[{0}] = -4;
+
+    LinAlg::Tensor<float> A_exp {{1, 2}, 1};
+    A_exp[{0, 0}] = -4;
+
+    EXPECT_EQ(A, A_exp);
+}
+
+TEST(Tensor, TransposeIsView) {
+    LinAlg::Tensor<float> A {{2, 2}, 1};
+    LinAlg::Tensor<float> A_t {A.t()};
+    A_t[{0, 1}] = -4;
+
+    LinAlg::Tensor<float> A_exp {{2, 2}, 1};
+    A_exp[{1, 0}] = -4;
+
+    EXPECT_EQ(A, A_exp);
+}
+
+TEST(Tensor, RowThenUnsqueeze) {
+    LinAlg::Tensor<float> A {{2, 2}};
+    LinAlg::Tensor<float> A_ru {A.row(1).unsqueeze()};
+
+    A_ru[{0, 1}] = 8;
+
+    LinAlg::Tensor<float> A_exp {{2, 2}};
+    A_exp[{1, 1}] = 8;
+
+    EXPECT_EQ(A, A_exp);
+}
+
+TEST(Tensor, SumOfRowView) {
+    LinAlg::Tensor<float> A {{2, 2}, 0};
+    A[{0, 1}] = 8;
+    A[{0, 0}] = -1;
+    A[{1, 0}] = 27;
+
+    EXPECT_EQ(7, A.row(0).sum());
+}
+
+TEST(Tensor, MaxOfRowView) {    
+    LinAlg::Tensor<float> A {{2, 2}, 0};
+    A[{0, 1}] = 8;
+    A[{0, 0}] = -1;
+    A[{1, 0}] = 27;
+
+    EXPECT_EQ(8, A.row(0).max());
+}
+
+TEST(Tensor, MatmulTransposedView) {
+    LinAlg::Tensor<float> A {{2, 2}, 0};
+    A[{0, 0}] = 2;
+    A[{0, 1}] = 4;
+    A[{1, 1}] = -1;
+
+    LinAlg::Tensor<float> B {{2, 2}, 1};
+    B[{1, 0}] = 4;
+
+    LinAlg::Tensor<float> C {A * B.t()};
+
+    LinAlg::Tensor<float> C_exp {{2, 2}, 6};
+    C_exp[{0, 1}] = 12;
+    C_exp[{1, 0}] = -1;
+    C_exp[{1, 1}] = -1;
+
+    EXPECT_EQ(C, C_exp);
+}
+
+TEST(Tensor, MatmulRankLessThanTwoThrows) {
+    LinAlg::Tensor<float> A {{2}};
+    LinAlg::Tensor<float> B {{2, 2}};
+
+    EXPECT_THROW(A * B, std::invalid_argument);
+}
+
+TEST(Tensor, MatmulInnerDimMismatchThrows) {
+    LinAlg::Tensor<float> A {{2, 2}};
+    LinAlg::Tensor<float> B {{3, 2}};
+
+    EXPECT_THROW(A * B, std::invalid_argument);
+}
+
+TEST(Tensor, MatmulBatchAxisMismatchThrows) {
+    LinAlg::Tensor<float> A {{2, 2, 2}};
+    LinAlg::Tensor<float> B {{3, 2, 2}};
+
+    EXPECT_THROW(A * B, std::invalid_argument);
+}
+
+TEST(Tensor, GetExtentAxisOutOfRangeThrows) {
+    LinAlg::Tensor<float> A {{2, 2}};
+
+    EXPECT_THROW(A.get_extent(2), std::invalid_argument);
+    EXPECT_THROW(A.get_extent(-1), std::invalid_argument);
+}
+
+TEST(Tensor, RowRankOneThrows) {
+    LinAlg::Tensor<float> A {{3}};
+
+    EXPECT_THROW(A.row(1), std::invalid_argument);
+}
+
+TEST(Tensor, RowIndexOutOfRangeThrows) {
+    LinAlg::Tensor<float> A{{3, 2}};
+
+    EXPECT_THROW(A.row(3), std::invalid_argument);
+    EXPECT_THROW(A.row(-1), std::invalid_argument);
+}
+
+TEST(Tensor, UsqueezeAxisOutOfRangeThrows) {
+    LinAlg::Tensor<float> A {{3, 2}};
+
+    EXPECT_NO_THROW(A.unsqueeze(2));
+    EXPECT_THROW(A.unsqueeze(-1), std::invalid_argument);
+    EXPECT_THROW(A.unsqueeze(3), std::invalid_argument);
+}
+
+TEST(Tensor, SqueezeRankOneThrows) {
+    LinAlg::Tensor<float> A {{2}};
+
+    EXPECT_THROW(A.squeeze(), std::invalid_argument);
+}
+
+TEST(Tensor, SqueezeAxisOutOfRangeThrows) {
+    LinAlg::Tensor<float> A {{1, 1}};
+
+    EXPECT_THROW(A.squeeze(-1), std::invalid_argument);
+    EXPECT_THROW(A.squeeze(2), std::invalid_argument);
+}
+
+TEST(Tensor, SqueezeExtentNotOneThrows) {
+    LinAlg::Tensor<float> A {{2, 2}};
+
+    EXPECT_THROW(A.squeeze(), std::invalid_argument);
+}
+
+TEST(Tensor, TransposeRankOneThrows) {
+    LinAlg::Tensor<float> A {{2}};
+
+    EXPECT_THROW(A.t(), std::invalid_argument);
+}
+
+TEST(Tensor, ConstructorInitValues) {
+    LinAlg::Tensor<float> A {{3, 2}, 4};
+
+    for(int i {}; i < 3; ++i) {
+        for(int j {}; j < 2; ++j) {
+            float element {A[{i, j}]};
+            EXPECT_EQ(4, element);
+        }
+    }
+}
+
+TEST(Tensor, SetAllElements) {
+    LinAlg::Tensor<float> A {{2, 3}, 1};
+    A.set_all_elements(-10);
+
+    for(int i {}; i < 2; ++i) {
+        for(int j {}; j < 3; ++j) {
+            float element {A[{i, j}]};
+            EXPECT_EQ(-10, element);
+        }
+    }
+}
+
+TEST(Tensor, GetExtent) {
+    LinAlg::Tensor<float> A {{2, 3}};
+
+    EXPECT_EQ(A.get_extent(0), 2);
+    EXPECT_EQ(A.get_extent(1), 3);
+}
+
+TEST(Tensor, NormalFillsAllElements) {
+    LinAlg::Tensor<float> A {{2, 3}, 10};
+    Rand::Random<float> random {42};
+
+    A.normal(random, -3, 1);
+
+    for(int i {}; i < 2; ++i) {
+        for(int j {}; j < 3; ++j) {
+            float element {A[{i, j}]};
+            EXPECT_NE(element, 10);
+        }
+    }
+
+    float first {A[{0, 0}]};
+    float second {A[{1, 2}]};
+
+    EXPECT_NE(first, second);
+}
+
+TEST(Tensor, UniformWithinBounds) {
+    LinAlg::Tensor<float> A {{2, 2}, 10};
+    Rand::Random<float> random {42};
+
+    A.uniform(random, 0, 3);
+
+    for(int i {}; i < 2; ++i) {
+        for(int j {}; j < 2; ++j) {
+            float element {A[{i, j}]};
+            EXPECT_TRUE(element >= 0 && element <= 3);
+        }
+    }
+}
+
+TEST(Tensor, ToString) {
+    LinAlg::Tensor<float> A {{2, 3}, -3};
+    A[{0, 0}] = 8;
+
+    std::string A_str {static_cast<std::string>(A)};
+
+    EXPECT_EQ(A_str, "(2, 3)");
+}
+
+TEST(Tensor, PairwiseAddFreeFunction) {
+    LinAlg::Tensor<float> A {{2, 3}, -1};
+    A[{0, 1}] = 8;
+
+    LinAlg::Tensor<float> B {{2, 3}, 5};
+    B[{1, 2}] = 2;
+    B[{0, 1}] = 1;
+
+    LinAlg::Tensor<float> C {LinAlg::pairwise_add<float>(A, B)};
+    LinAlg::Tensor<float> C_exp {{2, 3}, 4};
+    C_exp[{0, 1}] = 9;
+    C_exp[{1, 2}] = 1;
+    
+    EXPECT_EQ(C, C_exp);
+}
+
+TEST(Tensor, PairwiseSubFreeFunction) {
+    LinAlg::Tensor<float> A {{2, 3}, -1};
+    A[{0, 1}] = 8;
+
+    LinAlg::Tensor<float> B {{2, 3}, 5};
+    B[{1, 2}] = 2;
+    B[{0, 1}] = 1;
+
+    LinAlg::Tensor<float> C {LinAlg::pairwise_sub<float>(A, B)};
+    LinAlg::Tensor<float> C_exp {{2, 3}, -6};
+    C_exp[{0, 1}] = 7;
+    C_exp[{1, 2}] = -3;
+    
+    EXPECT_EQ(C, C_exp);    
 }
