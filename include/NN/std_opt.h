989f1676d1ac50cfe635a56e603ba5ef03feb275
RubenAnneBonny <ruben.rystedt@icloud.com>
2026-07-22 18:39:19 +0200

    Added adam optimizer + tests

--- include/NN/std_opt.h
+++ include/NN/std_opt.h
@@ -7,7 +7,7 @@
 #include <vector>
 
 namespace NN {
-    template <typename T>
+    template <std::floating_point T>
     class Gradient_descent {
         private:
             T m_lr;
@@ -25,6 +25,75 @@ namespace NN {
             void step() {
                 for(auto* param : m_parameters) {
                     param->value -= param->grad * m_lr;
+                }
+            }
+    };
+
+    template <std::floating_point T>
+    class Adam {
+        private:
+            T m_beta_1;
+            T m_beta_2;
+            T m_alpha;
+            T m_eps;
+            std::vector<NN::Parameter<T>*> m_parameters;
+            std::vector<LinAlg::Tensor<T>> m_first_moment;
+            std::vector<LinAlg::Tensor<T>> m_second_moment;
+            int m_t;
+
+        public:
+            Adam(T beta_1 = 0.9, T beta_2 = 0.999, T alpha = 1e-3, T eps = 1e-8)
+                : m_beta_1 {beta_1}
+                , m_beta_2 {beta_2}
+                , m_alpha {alpha}
+                , m_eps {eps}
+                , m_first_moment {}
+                , m_second_moment {}
+                , m_t {}
+            {}
+            
+            void init(std::vector<NN::Parameter<T>*>& parameters) {
+                m_parameters = parameters;
+
+                for(int i {}; i < static_cast<int>(m_parameters.size()); ++i) {
+                    LinAlg::Tensor<T> X {{m_parameters[i]->value.get_extent(0), m_parameters[i]->value.get_extent(1)}};
+
+                    m_first_moment.push_back(X.copy());
+                    m_second_moment.push_back(X);
+                }
+            }
+
+            void step() {
+                ++m_t;
+
+                for(int i {}; i < static_cast<int>(m_parameters.size()); ++i) {
+                    auto first{
+                        [&](T a, T b)
+                        {
+                            return m_beta_1 * a + (1 - m_beta_1) * b;
+                        }
+                    };
+                    auto second{
+                        [&](T a, T b)
+                        {
+                            return m_beta_2 * a + (1 - m_beta_2) * b * b;
+                        }
+                    };
+
+                    m_first_moment[i] = LinAlg::pairwise<T>(m_first_moment[i], m_parameters[i]->grad, first);
+                    m_second_moment[i] = LinAlg::pairwise<T>(m_second_moment[i], m_parameters[i]->grad, second);
+
+                    LinAlg::Tensor<T> first_hat = m_first_moment[i] * (1 / (1 - std::pow(m_beta_1, m_t)));
+                    LinAlg::Tensor<T> second_hat = m_second_moment[i] * (1 / (1 - std::pow(m_beta_2, m_t)));
+
+                    auto step_size {
+                        [&](T a, T b)
+                        {
+                            return m_alpha * a / (std::sqrt(b) + m_eps);
+                        }
+                    };
+
+                    m_parameters[i]->value = m_parameters[i]->value - LinAlg::pairwise<T>(first_hat, second_hat, step_size);
                 }
             }
     };
--- tests/NN/std_opt_test.cpp
+++ tests/NN/std_opt_test.cpp
@@ -35,3 +35,59 @@ TEST(Optimizer, GDHandcomputedStep) {
 
     EXPECT_EQ(param.value, val_exp);
 }
+
+TEST(Optimizer, AdamOptimizerCheck) {
+    NN::Adam<double> adam {};
+
+    Debug::Optimizer_result result {Debug::optimizer_check<NN::Adam<double>>(adam)};
+
+    EXPECT_TRUE(result.passed);
+}
+
+TEST(Optimizer, AdamHandcomputedStep) {
+    NN::Parameter<float> param {{1, 3}, 1};
+    param.value[{0, 1}] = 5;
+    param.value[{0, 2}] = -2;
+
+    param.grad[{0, 0}] = 8;
+    param.grad[{0, 2}] = -5;
+
+    NN::Adam<float> adam {0.9f, 0.999f, 0.5f, 1e-8f};
+    
+    std::vector<NN::Parameter<float>*> params;
+    params.push_back(&param);
+
+    adam.init(params);
+    adam.step();
+
+    LinAlg::Tensor<float> value_exp {{1, 3}, 0.5f};
+    value_exp[{0, 1}] = 5;
+    value_exp[{0, 2}] = -1.5f;
+
+    EXPECT_TRUE(LinAlg::all_close<float>(param.value, value_exp, 1e-5f, 1e-5f));
+}
+
+TEST(Optimizer, AdamMultipleSteps) {
+    NN::Parameter<float> param {{1, 3}, 1};
+    param.value[{0, 1}] = 5;
+    param.value[{0, 2}] = -2;
+
+    param.grad[{0, 0}] = 8;
+    param.grad[{0, 2}] = -5;
+
+    NN::Adam<float> adam {0.9f, 0.999f, 0.5f, 1e-8f};
+    
+    std::vector<NN::Parameter<float>*> params;
+    params.push_back(&param);
+
+    adam.init(params);
+    adam.step();
+    adam.step();
+    adam.step();
+
+    LinAlg::Tensor<float> value_exp {{1, 3}, -0.5f};
+    value_exp[{0, 1}] = 5;
+    value_exp[{0, 2}] = -0.5f;
+
+    EXPECT_TRUE(LinAlg::all_close<float>(param.value, value_exp, 1e-5f, 1e-5f));
+}
