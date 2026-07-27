e65918cb13832e2365051e535d9ec85b0e10f34c
RubenAnneBonny <ruben.rystedt@icloud.com>
2026-07-27 19:41:58 +0200

    Create a cml.h umberalla include for the library

---
+++ include/cml.h
@@ -0,0 +1,15 @@
+#ifndef CML_H
+#define CML_H
+
+#include <LinAlg/tensor.h>
+#include <Rand/random.h>
+#include <Func/std_func.h>
+#include <NN/std_opt.h>
+#include <NN/layer.h>
+#include <NN/model.h>
+#include <Data/data_loader.h>
+#include <Data/data_generation.h>
+#include <Debug/gradient_check.h>
+#include <Debug/optimizer_check.h>
+
+#endif
